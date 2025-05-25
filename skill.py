c3ae73b124ec300dd0280d3395f06bce2614f574
shuaibi-12138 <1462759334@qq.com>
2025-05-25 13:25:18 +1000

    skill.py

--- skill.py
+++ skill.py
@@ -2,4 +2,4 @@ class Skill:
     def __init__(self, name, power, status_effect=None):
         self.name = name
         self.power = power
-        self.status_effect = status_effect  # e.g. "poison", "paralysis"
+        self.status_effect = status_effect  
